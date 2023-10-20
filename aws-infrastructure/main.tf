18c5df05542feb3286185c0b51eda7f8d980e0ec
yv@aws <shreeshree3@gmail.com>
2023-10-20 13:17:37 -0500

    testing TF version controlling

--- aws-infrastructure/main.tf
+++ aws-infrastructure/main.tf
@@ -6,13 +6,23 @@ terraform {
     }
   }
 
-  backend "s3" {
+  required_version = ">=1.2.0"
 
-    bucket = "terraform-backend-yv"
-    key = "terraform.tfstate"
-    region = "us-east-1"
+  ### Infrastructure is continuously evolving, and Terraform helps you manage that change. 
+  ### As you change Terraform configurations, 
+  ### Terraform builds an execution plan that only modifies what is necessary to reach your desired state.
+
+  ### When using Terraform in production, we recommend that you use a version control system 
+  ### to manage your configuration files, 
+  ### and store your state in a remote backend such as Terraform Cloud or Terraform Enterprise.
+
+  # backend "s3" {
+
+  #   bucket = "terraform-backend-yv"
+  #   key = "terraform.tfstate"
+  #   region = "us-east-1"
     
-  }
+  # }
 }
 
 
@@ -20,18 +30,18 @@ provider "aws" {
   region     = "us-east-1"
 }
 
-# resource "aws_instance" "this" {
-#   ami                     = var.ami
-#   instance_type           = var.instance_type
-#   subnet_id               = var.subnet_id
-# }
-
-
-resource "aws_s3_bucket" "example" {
-  bucket = "my-tf-test-bucket02-yv"
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
+resource "aws_instance" "this" {
+  ami                     = var.ami
+  instance_type           = var.instance_type
+  subnet_id               = var.subnet_id
 }
+
+
+# resource "aws_s3_bucket" "example" {
+#   bucket = "my-tf-test-bucket02-yv"
+
+#   tags = {
+#     Name        = "My bucket"
+#     Environment = "Dev"
+#   }
+# }
