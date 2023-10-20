13ee4f741811cf54928c00afec5f6dcabe68d1cc
yv@aws <shreeshree3@gmail.com>
2023-10-20 13:21:24 -0500

    changes made to output.tf file

--- aws-infrastructure/output.tf
+++ aws-infrastructure/output.tf
@@ -1,14 +1,29 @@
-output "bucket_name" {
-  value = aws_s3_bucket.example.bucket
-  description = "the name of the AWS S3 bucket"
+# output "bucket_name" {
+#   value = aws_s3_bucket.example.bucket
+#   description = "the name of the AWS S3 bucket"
+# }
+
+# output "bucket_arn" {
+#     value = aws_s3_bucket.example.arn
+#     description = "The Amazon Resource name (ARN) for a S3 bucket"
+# }
+
+# output "bucket_region" {
+#   value = aws_s3_bucket.example.region
+#   description = "The AWS region in which the S3 bucket is located"
+# }
+
+output "aws_instance" {
+  value = aws_instance.this.ami
+  description = "AMI for the EC2 instance "
 }
 
-output "bucket_arn" {
-    value = aws_s3_bucket.example.arn
-    description = "The Amazon Resource name (ARN) for a S3 bucket"
+output "instance_type" {
+  value = aws_instance.this.instance_type
+  description = "Type of the EC2 instance created"
 }
 
-output "bucket_region" {
-  value = aws_s3_bucket.example.region
-  description = "The AWS region in which the S3 bucket is located"
+output "subnet_id" {
+  value = aws_instance.this.subnet_id
+  description = "ID of the Subnet where the EC2 instance is created"
 }
