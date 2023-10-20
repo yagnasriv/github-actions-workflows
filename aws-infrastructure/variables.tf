5e7f699d9c290df613cc430da3d47c9dd599660c
yv@aws <shreeshree3@gmail.com>
2023-10-20 13:28:56 -0500

    changes made to ami variable

--- aws-infrastructure/variables.tf
+++ aws-infrastructure/variables.tf
@@ -1,6 +1,7 @@
 variable "ami" {
   description = "instance os type"
-  default = "ami-053b0d53c279acc90"
+  #default = "ami-053b0d53c279acc90" ## AMI for ubuntu instance
+  default = "ami-0df435f331839b2d6" ## AMI for Amazon Linux Instance
 }
 
 variable "instance_type" {
